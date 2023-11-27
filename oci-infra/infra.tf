10e0fe96c2abdb529b4b7873a8d08ddac48690f2
bri <284789+b-@users.noreply.github.com>
2023-11-26 22:24:18 -0500

    add vcn

--- oci-infra/infra.tf
+++ oci-infra/infra.tf
@@ -21,3 +21,139 @@ module "vcn" {
   create_nat_gateway      = true
   create_service_gateway  = true
 }
+
+resource "oci_core_security_list" "public_subnet_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+
+  display_name = "free-k8s-public-subnet-sl"
+
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+
+  ingress_security_rules {
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol    = "6"
+    tcp_options {
+      min = 6443
+      max = 6443
+    }
+  }
+}
+
+resource "oci_core_security_list" "private_subnet_sl" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+
+  display_name = "free-k8s-private-subnet-sl"
+
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+  ingress_security_rules {
+    stateless   = false
+    source      = "10.0.0.0/16"
+    source_type = "CIDR_BLOCK"
+    protocol    = "all"
+  }
+}
+
+resource "oci_core_subnet" "vcn_private_subnet" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  cidr_block     = "10.0.1.0/24"
+
+  route_table_id             = module.vcn.nat_route_id
+  security_list_ids          = [oci_core_security_list.private_subnet_sl.id]
+  display_name               = "free-k8s-private-subnet"
+  prohibit_public_ip_on_vnic = true
+}
+
+resource "oci_core_subnet" "vcn_public_subnet" {
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  cidr_block     = "10.0.0.0/24"
+
+  route_table_id    = module.vcn.ig_route_id
+  security_list_ids = [oci_core_security_list.public_subnet_sl.id]
+  display_name      = "free-k8s-public-subnet"
+}
+
+resource "oci_containerengine_cluster" "k8s_cluster" {
+  compartment_id     = var.compartment_id
+  kubernetes_version = "v1.27.2"
+  name               = "free-k8s-cluster"
+  vcn_id             = module.vcn.vcn_id
+
+  endpoint_config {
+    is_public_ip_enabled = true
+    subnet_id            = oci_core_subnet.vcn_public_subnet.id
+  }
+
+  options {
+    add_ons {
+      is_kubernetes_dashboard_enabled = false
+      is_tiller_enabled               = false
+    }
+    kubernetes_network_config {
+      pods_cidr     = "10.244.0.0/16"
+      services_cidr = "10.96.0.0/16"
+    }
+    service_lb_subnet_ids = [oci_core_subnet.vcn_public_subnet.id]
+  }
+}
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.compartment_id
+}
+resource "oci_containerengine_node_pool" "k8s_node_pool" {
+  cluster_id         = oci_containerengine_cluster.k8s_cluster.id
+  compartment_id     = var.compartment_id
+  kubernetes_version = "v1.27.2"
+  name               = "free-k8s-node-pool"
+  node_config_details {
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+      subnet_id           = oci_core_subnet.vcn_private_subnet.id
+    }
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+      subnet_id           = oci_core_subnet.vcn_private_subnet.id
+    }
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
+      subnet_id           = oci_core_subnet.vcn_private_subnet.id
+    }
+    size = 2
+  }
+  node_shape = "VM.Standard.A1.Flex"
+  node_shape_config {
+    memory_in_gbs = 6
+    ocpus         = 1
+  }
+  node_source_details {
+    image_id    = "ocid1.image.oc1.iad.aaaaaaaamo3amvibu2izgrng5zs4u34yw5y2g2v3v5l4fxvbeezepcsren7q"
+    source_type = "image"
+  }
+  initial_node_labels {
+    key   = "name"
+    value = "free-k8s-cluster"
+  }
+  ssh_public_key = var.ssh_public_key
+}
